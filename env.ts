ce78ff831a1a75d2c06fea48aef81baad8789235
Dariusz Filipiak <darekf77@gmail.com>
2025-04-08 20:28:14 +0200

    release: v18.0.21

---
+++ env.ts
@@ -0,0 +1,9 @@
+import type { EnvOptions } from 'tnp/src';
+
+const env: Partial<EnvOptions> = {
+  website: {
+    domain: 'taon-type-sql.example.domain.com',
+    title: 'Taon Type Sql',
+  },
+};
+export default env;
